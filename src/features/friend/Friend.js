0560e0e3706aa419deb4d4194d83a818f5553c8b
gomjellie <gomjellie@gmail.com>
2021-05-02 20:48:29 +0900

    ui: Friends Search Input 완성

--- src/features/friend/Friend.js
+++ src/features/friend/Friend.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import addFriend from '../../assets/addFriend.png';
+import addFriendImg from '../../assets/addFriend.png';
+import searchImg from '../../assets/search.png';
 
 export default function Friend() {
   return (
     <section className="ContentRoot">
       <header className="ContentHeader">
         <h4 className="TabName">Friends</h4>
-        <img id="addFriend" height="65%" src={addFriend} alt=""/>
+        <img id="addFriend" height="65%" src={addFriendImg} alt=""/>
         
       </header>
       <div className="ContentSearchBar">
-
+        <div class="SearchFriendsWrapper">
+          <img id="search" height="75%" src={searchImg} alt=""/>
+          <input className="SearchFriends" placeholder="Search Friends" />
+        </div>
       </div>
       <main>
 
