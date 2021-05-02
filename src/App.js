004df0a6dc8c4d9bcef3778cce6393b3a5d1358c
gomjellie <gomjellie@gmail.com>
2021-05-02 11:51:53 +0900

    scss style 구조 추가

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
-import './App.css';
+import './styles/style.css';
 
 function App() {
   return (
