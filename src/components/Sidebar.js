480c7281674dbe247c2e04210e8c8488e3154a60
gomjellie <gomjellie@gmail.com>
2021-05-03 14:33:35 +0900

    선택한 탭 흰색으로 하이라이트 표시

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import bellImg from '../assets/bell.png';
 import etcImg from '../assets/etc.png';
@@ -10,24 +10,28 @@ import { useHistory } from 'react-router-dom';
 
 export default function Sidebar() {
   const history = useHistory();
+  const [selected, setSelected] = useState('/');
 
   const pushFriend = () => {
+    setSelected('/');
     history.push('/');
   }
 
   const pushChat = () => {
+    setSelected('/chat');
     history.push('/chat');
   }
 
   const pushEtc = () => {
+    setSelected('/etc');
     history.push('/etc');
   }
   
   return (
     <aside className="Sidebar">
-      <img class="highlight" width="33%" id="personal" src={userImg} onClick={pushFriend} alt=""/>
-      <img width="33%" id="chat" src={chatImg} onClick={pushChat} alt=""/>
-      <img width="30%" id="etc" src={etcImg} onClick={pushEtc} alt=""/>
+      <img class={selected==='/' && "highlight"} width="33%" id="personal" src={userImg} onClick={pushFriend} alt=""/>
+      <img class={selected==='/chat' && "highlight"} width="33%" id="chat" src={chatImg} onClick={pushChat} alt=""/>
+      <img class={selected==='/etc' && "highlight"} width="30%" id="etc" src={etcImg} onClick={pushEtc} alt=""/>
       <img width="25%" id="bell" src={bellImg} alt=""/>
       <img width="25%" id="setting" src={settingImg} alt=""/>
     </aside>
