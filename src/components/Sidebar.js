001a9bd5e499bbff02a64e0b19bbcd649b9c607f
gomjellie <gomjellie@gmail.com>
2021-05-03 14:26:46 +0900

    router-dom 적용, 친구섹션, 채팅섹션 나눠서 라우팅

---
+++ src/components/Sidebar.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import bellImg from '../assets/bell.png';
+import etcImg from '../assets/etc.png';
+import settingImg from '../assets/setting.png';
+import chatImg from '../assets/chat.png';
+import userImg from '../assets/user.png';
+
+import { useHistory } from 'react-router-dom';
+
+export default function Sidebar() {
+  const history = useHistory();
+
+  const pushFriend = () => {
+    history.push('/');
+  }
+
+  const pushChat = () => {
+    history.push('/chat');
+  }
+
+  const pushEtc = () => {
+    history.push('/etc');
+  }
+  
+  return (
+    <aside className="Sidebar">
+      <img class="highlight" width="33%" id="personal" src={userImg} onClick={pushFriend} alt=""/>
+      <img width="33%" id="chat" src={chatImg} onClick={pushChat} alt=""/>
+      <img width="30%" id="etc" src={etcImg} onClick={pushEtc} alt=""/>
+      <img width="25%" id="bell" src={bellImg} alt=""/>
+      <img width="25%" id="setting" src={settingImg} alt=""/>
+    </aside>
+  )
+}
---
+++ src/features/chat/Chat.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Chat() {
+
+  return (
+    <div className="Chat">
+      Chat
+    </div>
+  )
+}
