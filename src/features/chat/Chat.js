3a271e9d1240337f812ab701731e3bae654b788f
gomjellie <gomjellie@gmail.com>
2021-05-03 20:21:25 +0900

    Chats title, searchbar

--- src/features/chat/Chat.js
+++ src/features/chat/Chat.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import addChatImg from '../../assets/addChat.png';
+import searchImg from '../../assets/search.png';
 
 export default function Chat() {
-
   return (
-    <div className="Chat">
-      Chat
-    </div>
+    <section className="ContentRoot">
+      <header className="ContentHeader">
+        <div className="TabName">Chats</div>
+        <img id="addChat" height="70%" src={addChatImg} alt=""/>
+      </header>
+      <div className="ContentSearchBar">
+        <div className="SearchFriendsWrapper">
+          <img id="search" height="80%" src={searchImg} alt=""/>
+          <input className="SearchFriends" placeholder="Search chatrooms, participants" />
+        </div>
+      </div>
+    </section>
   )
 }
