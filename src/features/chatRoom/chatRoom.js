c41fa4595450f6fa9612f421ee7ac9b7a99d74d8
gomjellie <gomjellie@gmail.com>
2021-05-09 10:50:29 +0900

    chatroom inputtools 아이콘 추가, 배치

--- src/features/chatRoom/chatRoom.js
+++ src/features/chatRoom/chatRoom.js
@@ -1,4 +1,9 @@
 import React from "react";
+import EmoticonIcon from "assets/icons/EmoticonFace.png";
+import PaperClipIcon from "assets/icons/PaperClip.png";
+import CalendarCheckIcon from "assets/icons/CalendarCheck.png";
+import PhoneCallIcon from "assets/icons/PhoneCall.png";
+import VideoCallIcon from "assets/icons/VideoCall.png";
 
 export default function ChatRoom({ match }) {
   // const { id } = match.params;
@@ -8,19 +13,18 @@ export default function ChatRoom({ match }) {
       <header className="Header">
         <div className="Title">ChatRoom</div>
       </header>
-      <main>
-        
-      </main>
+      <main></main>
       <div className="InputTools">
-
+        <img src={EmoticonIcon} alt="" />
+        <img src={PaperClipIcon} alt="" />
+        <img src={CalendarCheckIcon} alt="" />
+        <div className="ThisIsEmpty"></div>
+        <img src={PhoneCallIcon} alt="" />
+        <img src={VideoCallIcon} alt="" />
       </div>
       <div className="Input">
-        <textarea className="TextInput" spellcheck="false">
-
-        </textarea>
-        <div className="SendButton">
-          Send
-        </div>
+        <textarea className="TextInput" spellcheck="false"></textarea>
+        <div className="SendButton">Send</div>
       </div>
     </section>
   );
